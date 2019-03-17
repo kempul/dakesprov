882f8d1ba2519c7bea514cd015333f0e8cfcdf55
kempul <haidarfaz@gmail.com>
2019-03-17 11:24:04 +0700

    update cron

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ cron.schedule("0 5 1 * *", function () {
     timezone: "Asia/Jakarta"
 });
 
-cron.schedule("0 5 15 * *", function() {
+cron.schedule("0 5 28 * *", function() {
     var tanggal = new Date();
     tanggal.setDate(tanggal.getDate() - 1);
     var kemarin = moment(tanggal, "DD MM YYYY, hh:mm:ss");
@@ -68,6 +68,7 @@ cron.schedule("0 5 15 * *", function() {
     console.log("bulan : " + bulan);
     console.log("tgl : " + tgl);
 
+    
     kunjunganRes.kirimKunjungan("put", "RAWAT JALAN", tahun, bulan);
     kunjunganRes.kirimKunjungan("put", "RAWAT INAP", tahun, bulan);
 
